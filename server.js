7cd8e280f88224c2d5bcebeae56bceba2df0370f
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-07-07 11:46:46 -0300

    jwt

--- server.js
+++ server.js
@@ -2,18 +2,32 @@ import express from "express";
 import cors from "cors"
 import MedicamentosRouter from "./ControllerMedicamento.js";
 import RecetasRouter from "./ControllerReceta.js";
+import authRouter from "./authController.js";
+import getSignedToken from "./authController.js";
+import passport from 'passport';
+import { jwtStrategy } from './common/jwt.strategy.js';
+import { Authenticate } from "./common/jwt.strategy.js";
 
 const app  = express(); 
 const port = 3000; 
-// Agrego los Middlewares 
 app.use(cors());
 app.use(express.json());
+passport.use(jwtStrategy);
+app.use(passport.initialize());
+
+app.use("/api/auth/",authRouter);
+
+
+app.post('/login' ,async (req,res) => {
+    const token = getSignedToken();
+    return res.status(200).json(token);
+})
+
+
 app.use("/api/medicamento/",MedicamentosRouter);
 app.use("/api/receta/",RecetasRouter);
 
 app.use(express.static('public'));
-
-//localhost:3000
 
 app.listen(port,()=>{
     console.log('listening on port');
