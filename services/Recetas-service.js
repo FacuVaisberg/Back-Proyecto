60a89a22220eb2b3291f41a8d7dd5cc7bb633892
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-27 12:03:38 -0300

    fod

--- services/Recetas-service.js
+++ services/Recetas-service.js
@@ -12,7 +12,7 @@ export default class RecetaService
                 let result = await pool.request()
                 .query('select * from receta where IdReceta not in (select IdReceta from Solicitudes)');
                 result = await pool.request()
-                .query('SELECT M.NombreMedicamento, UM.Nombre as NombreMedico, UF.Nombre as NombreFarmacia, UP.Nombre as NombrePaciente, FechaCreacion, P.Direccion, P.Dni FROM Receta R   INNER JOIN Medicamentos M ON R.IdMedicamento = M.IdMedicamentos   INNER JOIN Usuario UM ON R.IdMedico = UM.IdUsuario   INNER JOIN Usuario UF ON R.IdFarmacia = UF.IdUsuario   INNER JOIN Usuario UP ON R.IdPaciente = UP.IdUsuario   INNER JOIN Paciente P ON P.IdUsuario = UP.IdUsuario')
+                .query('SELECT M.NombreMedicamento, UM.Nombre as NombreMedico, UF.Nombre as NombreFarmacia, UP.Nombre as NombrePaciente, FechaCreacion, P.Direccion, P.Dni, U.Telefono FROM Receta R INNER JOIN Medicamentos M ON R.IdMedicamento = M.IdMedicamentos INNER JOIN Usuario UM ON R.IdMedico = UM.IdUsuario INNER JOIN Usuario UF ON R.IdFarmacia = UF.IdUsuario INNER JOIN Usuario UP ON R.IdPaciente = UP.IdUsuario INNER JOIN Paciente P ON P.IdUsuario = UP.IdUsuario INNER JOIN Usuario U  ON U.IdUsuario = UP.IdUsuario')
                 returnEntity = result.recordsets[0];
                 console.log(returnEntity)
             } catch (error) {
